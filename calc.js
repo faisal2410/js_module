d02883cfa61e1eb6bfb7cb29f3f416934d4e11f2
Faisal ahmed <faisal2410@yahoo.com>
2023-08-14 20:53:43 +0600

    initial commit

---
+++ calc.js
@@ -0,0 +1,15 @@
+// export const sum=(a,b)=>{
+//     return a + b;
+// }
+// export const sub=(a,b)=>{
+//     return a - b;
+// }
+ const sum=(a,b)=>{
+    return a + b;
+}
+ const sub=(a,b)=>{
+    return a - b;
+}
+
+
+export {sum,sub}
---
+++ whoami.js
@@ -0,0 +1,13 @@
+const sayMyName = () => {
+    return 'Faisal ahmed';
+}
+
+
+export default sayMyName;
+
+
+// export default function sayMyName () {
+//     return 'Faisal ahmed';
+// }
+
+
